2fbab865a70a743b5d235bc59768fc45b909f9ff
Oliver Tansley <84975755+OliverTansley@users.noreply.github.com>
2024-07-12 21:08:27 +0100

    Initial Commit

---
+++ Taskit/TaskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskitApp.swift
+//  Taskit
+//
+//  Created by Oliver tansley on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
